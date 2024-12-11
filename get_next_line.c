03deb3a9e1053969a14687af8e4598b2861fd644
sousuke-code <msousuke1@gmail.com>
2024-12-12 00:47:52 +0900

    改行までの読み取りを行う関数の実装

--- get_next_line.c
+++ get_next_line.c
@@ -8,32 +8,64 @@
 
 #define BUFF_SIZE 246
 
-int find_next_line(char *buff, int size)
+
+size_t ft_strlcpy(char *dst, const char *src, size_t size)
 {
-  int i;
-  i = 0;
-  while (i < size)
-  {
-    if (buff[i] == '\n')
-      return i;
-    i++;
-  }
-  return -1; //次の改行が見つからない場合
+    // src からdistに対して、size - 1バイトを複写する。
+    size_t n;
+    n = 0;
+    while (src[n])
+    { 
+      n++;  
+    }
+    if (size == 0) {
+      return n;
+    }
+    size_t i = 0; //戻り地で返すsrcの長さ
+    while (i  < size -1 && *src != '\0')
+    {
+        *dst = *src;
+        dst++;
+        src++;
+        i++;
+    }
+
+    *dst = '\0'; 
+    return n;
 }
 
+
+char *extra_line(*buff) //余分な行を取り除く
+{
+    int i;
+    char *buff_line;
+    i = 0;
+    while (buff[i] != '\n')
+    {
+        i++;
+    }
+    buff_line = malloc(i);
+    ft_strlcpy(buff_line, buff, i);
+    return buff;
+}
 
 
 char *get_next_line(int fd) //全ての読み込みを行う
 {
     size_t buff_end;
+    static *reminder;
     char *buff;
+    char *return_buff;
     int i;
     i = 0;
     buff = malloc(BUFF_SIZE);
     if (!buff)
       return NULL;
-    read(fd, buff, BUFF_SIZE); //buffにBUFF_SIZE分だけが読み込まれている状態
-    return buff;
+    read(fd, buff, BUFF_SIZE); 
+    return_buff = extra_line(buff);
+
+    
+    return return_buff;
 }
 
 int main(void)
