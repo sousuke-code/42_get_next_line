2cbcfd977a13e4591acd431c25a43efa48f9795a
sousuke-code <msousuke1@gmail.com>
2025-01-04 16:17:36 +0900

    テストケースの作成

---
+++ main_test.c
@@ -0,0 +1,49 @@
+
+// *mandantory
+// include <stdio.h>
+// int main()
+// {
+//     int fd;
+//     char *line;
+//     int i;
+//     i = 1;
+//     fd = open("", O_RDONLY);
+//     while((line = get_next_line(fd)) != NULL) {
+//         printf("%d回目の結果: %s\n", i, line);
+//         i++;
+//         free(line);
+//     }
+//     close fd
+// }
+
+
+
+// *bonus
+int main(void)
+{
+    int fd[4];
+    char *line;
+    int i;
+    int j;
+
+    j = 0;
+    i = 1;
+    fd[0] = open("",O_RDONLY);
+    fd[1] = open("",O_RDONLY);
+    fd[2] = open("",O_RDONLY);
+    fd[3] = open("",O_RDONLY);
+    fd[4] = open("",O_RDONLY);
+    while(j <= 4)
+    {
+        i = 0;
+        while((line = get_next_line(fd[j])) != NULL);
+        {
+            printf("%d行目の結果:%s\n", i, line);
+            i++;
+            free(line);
+        }
+        printf("----%d回目のファイルの読み込み完了---\n",j);
+        close(fd[j]);
+        j++;
+    }
+}
